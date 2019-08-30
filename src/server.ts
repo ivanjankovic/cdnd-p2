056b9ab3d1bdb6f2be4e4302e8470b5bbde113e2
ivan Jankovic <jankoemail@gmail.com>
2019-08-30 11:04:01 -0400

    Added deploy artifact to  config.yml

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import {isURL} from 'validator';
 
 (async () => {
 
@@ -13,23 +14,25 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get( "/filteredimage", async (req, res) => {
 
-  /**************************************************************************** */
+    const imgURL = req.query.image_url;
 
-  //! END @TODO1
+    if (!imgURL) {
+      return res.status(400).send("URL missing");
+    } else if (!isURL(imgURL)) {
+      return res.status(400).send("URL not correct");
+    }
+
+    return filterImageFromURL(imgURL).then( (imgPath) => {
+      res.status(200);
+      res.sendFile(imgPath);
+      res.on('finish', () => deleteLocalFiles([imgPath]));
+    })
+    .catch( (err) => {
+      res.status(500).send("Server error");
+    })
+  });
   
   // Root Endpoint
   // Displays a simple message to the user
